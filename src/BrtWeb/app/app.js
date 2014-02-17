b6208b7314e583c77e6cee14c36e62d4a4ab50a6
Muhammad Afzal Qureshi <afzal.qureshi@outlook.com>
2014-02-17 16:09:36 +0530

    trying

--- src/BrtWeb/app/app.js
+++ src/BrtWeb/app/app.js
@@ -3,8 +3,8 @@
     var app = angular.module('app', ['ngAnimate', // for angular animations
         'ngRoute', // for angular routing
         'ngSanitize', // for html markup cleanup used in side bar
-        'ui.bootstrap', // for bootstrap ui   
-        'services' // app services module
+        'ui.bootstrap' // for bootstrap ui   
+        //'services' // app services module
     ]);
     app.constant('AppConfig', {        
         baseUrl : 'http://azurewebsites.net'
